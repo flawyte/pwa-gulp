5758c8744aa328ac2ac994eefb1ec530ddff02f3
Mickael Allonneau <mickael.allonneau@gmail.com>
2017-09-08 19:18:02 +0200

    Project update

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-// Service Worker is generated at build time and saved to 'build/default/sw.js'
+importScripts('workbox-sw.prod.v1.3.0.js');
+
+const workboxSW = new self.WorkboxSW();
+workboxSW.precache([]);
+
+// Custom code
+
+self.addEventListener('install', function(event) {
+  if (typeof self.skipWaiting == 'function')
+    self.skipWaiting();
+});
